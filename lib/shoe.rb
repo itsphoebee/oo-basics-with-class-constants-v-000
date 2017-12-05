88e8c9f7ff12b366001b97e0dda705f211e5a6de
Phoebe Lau <phoebelau7@gmail.com>
2017-12-05 18:46:19 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?(brand) == false
-      BRANDS << brand
+    !BRANDS.include?(brand)? BRANDS << brand : nil
     end
   end
 
